b50a64ad0576ec0f6c1581903bb2292120768f6f
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-15 16:47:47 -0300

    implementa transferencia de saldo

--- views.py
+++ views.py
@@ -31,7 +31,23 @@ def desativar_conta(id):
         conta.status = Status.INATIVO
         session.commit()
 
+def transferir_saldo(conta_origem, conta_destino, valor):
+    with Session(engine) as session:
+        statement = select(Conta).where(Conta.id == conta_origem)
+        conta_origem = session.exec(statement).first()
+
+        statement = select(Conta).where(Conta.id == conta_destino)
+        conta_destino = session.exec(statement).first()
+
+        if conta_origem.valor < valor:
+            raise ValueError("Saldo insuficiente")
+
+        conta_origem.valor -= valor
+        conta_destino.valor += valor
+        session.commit()
+
 # conta = Conta(valor=0, banco=Bancos.ITAU)
 # criar_conta(conta)
 # print(listar_contas())
-desativar_conta(3)
+# desativar_conta(3)
+# transferir_saldo(1, 2, 10)
