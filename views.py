254f256390fa01940054c91a46d1ec43348d83da
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-15 17:06:29 -0300

    implementa movimentar dinheiro

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from sqlmodel import Session, select
-from models import Conta
+from models import Conta, Historico
 from connection import engine
-from enums import Bancos, Status
+from enums import Bancos, Status, Tipos
+from datetime import date
 
 def criar_conta(conta: Conta):
     with Session(engine) as session:
@@ -46,8 +47,29 @@ def transferir_saldo(conta_origem, conta_destino, valor):
         conta_destino.valor += valor
         session.commit()
 
+def movimentar_dinheiro(historico: Historico):
+    with Session(engine) as session:
+        statement = select(Conta).where(Conta.id == historico.conta_id)
+        conta = session.exec(statement).first()
+        # TODO: Validar se a conta está ativa
+        if conta.status == Status.INATIVO:
+            raise ValueError("Conta inativa")
+
+        if historico.tipos == Tipos.ENTRADA:
+            conta.valor += historico.valor
+        else:
+            if conta.valor < historico.valor:
+                raise ValueError("Saldo insuficiente")
+            conta.valor -= historico.valor
+        
+        session.add(historico)
+        session.commit()
+        return historico
+
 # conta = Conta(valor=0, banco=Bancos.ITAU)
 # criar_conta(conta)
 # print(listar_contas())
 # desativar_conta(3)
 # transferir_saldo(1, 2, 10)
+# historico = Historico(conta_id=2, tipos=Tipos.ENTRADA, valor=10, data=date.today())
+# movimentar_dinheiro(historico)
