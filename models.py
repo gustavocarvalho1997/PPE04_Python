37c1037b99f5b5993660ca2fd1b6f2fe0b09f900
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-15 01:31:07 -0300

    add __str__

--- models.py
+++ models.py
@@ -9,5 +9,8 @@ class Conta(SQLModel, table=True):
     banco: Bancos = Field(default=Bancos.NUBANK)
     status: Status = Field(default=Status.ATIVO)
 
+    def __str__(self):
+        return f"Conta {self.banco.value} - {self.valor} - {self.status.value}"
+
 if __name__ == "__main__":
     SQLModel.metadata.create_all(engine)
